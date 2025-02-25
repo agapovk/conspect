cec99785079709c6c73d44d63778bcbe3619ac3e
Konstantin Agapov <agapovk@AK.local>
2025-02-25 11:19:49 +0400

    save uploadthing file data to app db

--- src/app/api/uploadthing/core.ts
+++ src/app/api/uploadthing/core.ts
@@ -1,40 +1,50 @@
+import { db } from "@/server/db";
 import { auth } from "@clerk/nextjs/server";
 import { createUploadthing, type FileRouter } from "uploadthing/next";
 import { UploadThingError } from "uploadthing/server";
 
 const f = createUploadthing();
 
-// FileRouter for your app, can contain multiple FileRoutes
 export const ourFileRouter = {
-  // Define as many FileRoutes as you like, each with a unique routeSlug
   imageUploader: f({
     image: {
-      /**
-       * For full list of options and defaults, see the File Route API reference
-       * @see https://docs.uploadthing.com/file-routes#route-config
-       */
       maxFileSize: "4MB",
       maxFileCount: 1,
     },
   })
-    // Set permissions and file types for this FileRoute
     .middleware(async () => {
-      // This code runs on your server before upload
-
-      // const user = await auth(req);
       const user = await auth();
-
-      // If you throw, the user will not be able to upload
       // eslint-disable-next-line @typescript-eslint/only-throw-error
       if (!user.userId) throw new UploadThingError("Unauthorized");
 
-      // Whatever is returned here is accessible in onUploadComplete as `metadata`
       return { userId: user.userId };
     })
     .onUploadComplete(async ({ metadata, file }) => {
       // This code RUNS ON YOUR SERVER after upload
       console.log("Upload complete for userId:", metadata.userId);
       console.log("file url", file.ufsUrl);
+
+      const user = await db.user.findUnique({
+        where: {
+          clerkUserId: metadata.userId,
+        },
+      });
+
+      if (user) {
+        try {
+          await db.scheme.create({
+            data: {
+              url: file.ufsUrl,
+              key: file.key,
+              size: file.size,
+              name: file.name,
+              userId: user.id,
+            },
+          });
+        } catch (error) {
+          console.error("Error creating scheme in db", error);
+        }
+      }
 
       // !!! Whatever is returned here is sent to the clientside `onClientUploadComplete` callback
       return { uploadedBy: metadata.userId };
--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,17 +1,33 @@
 import Upload from "@/components/upload";
-import { auth, currentUser } from "@clerk/nextjs/server";
+import { db } from "@/server/db";
+import { auth } from "@clerk/nextjs/server";
 
 export default async function HomePage() {
   const { userId } = await auth();
   if (!userId) return <div>To show this page you need to be signed in</div>;
 
-  const user = await currentUser();
+  const user = await db.user.findUnique({
+    where: {
+      clerkUserId: userId,
+    },
+    include: {
+      Scheme: true,
+    },
+  });
+  if (!user) return <div>Could not find user</div>;
+
   return (
-    <div>
+    <div className="space-y-4">
       <div className="flex items-start justify-between">
-        Hello {user?.firstName ?? user?.emailAddresses[0]?.emailAddress}
+        Hello {user.firstName ?? user.email}
         <Upload />
       </div>
+      <h3 className="text-lg">User images</h3>
+      <ul>
+        {user.Scheme.map((s) => (
+          <li key={s.id}>- {s.name}</li>
+        ))}
+      </ul>
     </div>
   );
 }
